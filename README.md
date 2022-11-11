40d98319ca58153fc191c8ee408386f0adcfadcf
meealmathur23 <meenalmathur23@gmail.com>
2022-11-11 13:00:01 +0530

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # golang-apm-sql
+
+go get github.com/middleware-labs/golang-apm-sql
+
+```golang
+
+import (
+    mw_sql "github.com/middleware-labs/golang-apm-sql/sql"
+	"github.com/middleware-labs/golang-apm/tracker"
+)
+
+func main() {
+	tracker.Track(
+		track.WithConfigTag("service", "your service name"),
+		track.WithConfigTag("projectName", "your project name"),
+	)
+	db, err := mw_sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/todo")
+	if err != nil {
+		panic(err.Error())
+	}
+}
---
+++ example/example.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	_ "database/sql"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	g "github.com/middleware-labs/golang-apm-gin/gin"
+	mw_sql "github.com/middleware-labs/golang-apm-sql/sql"
+	"github.com/middleware-labs/golang-apm/tracker"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	config, _ := tracker.Track(
+		track.WithConfigTag("service", "Your service name"),
+		track.WithConfigTag("projectName", "Your project name"),
+	)
+	r.Use(g.Middleware(config))
+	db, err := mw_sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/todo")
+	if err != nil {
+		panic(err.Error())
+	}
+	r.GET("/todo", func(c *gin.Context) {
+		ctx := c.Request.Context()
+		tracker.SpanFromContext(ctx)
+		var num int
+		if err := db.QueryRowContext(ctx, "SELECT 42").Scan(&num); err != nil {
+			panic(err)
+		}
+		c.JSON(http.StatusOK, "ok")
+	})
+	_ = r.Run(":8081")
+}
