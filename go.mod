e41b3bd2a8525c295deb8b376d86c6587e8f4cad
meealmathur23 <meenalmathur23@gmail.com>
2023-04-10 16:20:22 +0530

    update with WithDBSystem & WithAttributes methods

--- go.mod
+++ go.mod
@@ -7,7 +7,8 @@ require github.com/uptrace/opentelemetry-go-extra/otelsql v0.1.21
 require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	go.opentelemetry.io/otel v1.13.0 // indirect
-	go.opentelemetry.io/otel/metric v0.36.0 // indirect
-	go.opentelemetry.io/otel/trace v1.13.0 // indirect
+	github.com/middleware-labs/opentelemetry-go-extra/otelsql v0.0.0-20230410102654-76cb5db38fb4 // indirect
+	go.opentelemetry.io/otel v1.14.0 // indirect
+	go.opentelemetry.io/otel/metric v0.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.14.0 // indirect
 )
--- sql/sql.go
+++ sql/sql.go
@@ -2,13 +2,22 @@ package sql
 
 import (
 	"database/sql"
-	"github.com/uptrace/opentelemetry-go-extra/otelsql"
+	"github.com/middleware-labs/opentelemetry-go-extra/otelsql"
+	"go.opentelemetry.io/otel/attribute"
 )
 
-func Open(driverName, dsn string) (*sql.DB, error) {
-	db, err := otelsql.Open(driverName, dsn)
+func Open(driverName, dsn string, opts ...otelsql.Option) (*sql.DB, error) {
+	db, err := otelsql.Open(driverName, dsn, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return db, nil
+}
+
+func WithDBSystem(system string) otelsql.Option {
+	return otelsql.WithDBSystem(system)
+}
+
+func WithAttributes(attrs ...attribute.KeyValue) otelsql.Option {
+	return otelsql.WithAttributes(attrs...)
 }
